3564f6f4c01069dc8f000ab2a45e68a418cc0591
Wojciech Bielecki <154003547+wojciechbiel@users.noreply.github.com>
2024-08-31 08:58:06 +0200

    Update login_test.py

--- tests/login_test.py
+++ tests/login_test.py
@@ -24,7 +24,7 @@ class LoginTest(BaseTest):
         login_page.enter_password(self.correct_data.correct_password)
         login_page.click_log_in()
         # check the message about incorrect user is displayed
-        self.assertEqual("Epic sadface: Username and password do not match any user in this service"
+        self.assertEqual(""Username or password incorrect"
                          , login_page.get_error_message())
 
     def test_invalid_password(self):
@@ -33,7 +33,7 @@ class LoginTest(BaseTest):
         login_page.enter_username(self.correct_data.correct_username)
         login_page.enter_password(self.test_data.password)
         login_page.click_log_in()
-        self.assertEqual("Epic sadface: Username and password do not match any user in this service"
+        self.assertEqual(""Username or password incorrect"
                          , login_page.get_error_message())
 
     def test_invalid_username_and_password(self):
@@ -41,7 +41,7 @@ class LoginTest(BaseTest):
         login_page.enter_username(self.test_data.user_name)
         login_page.enter_password(self.test_data.password)
         login_page.click_log_in()
-        self.assertEqual("Epic sadface: Username and password do not match any user in this service"
+        self.assertEqual(""Username or password incorrect"
                          , login_page.get_error_message())
 
     def test_valid_username_and_password(self):
